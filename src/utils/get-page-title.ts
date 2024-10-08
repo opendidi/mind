2fa3cafe17b2cad977697f719ce7c8f57697bb91
opendidi <lianghuitang@heneng.com>
2024-10-08 20:30:19 +0800

    修改错误描述

--- src/utils/get-page-title.ts
+++ src/utils/get-page-title.ts
@@ -4,11 +4,11 @@
  * @Author: htang
  * @Date: 2024-10-08 20:13:33
  * @LastEditors: htang
- * @LastEditTime: 2024-10-08 20:13:48
+ * @LastEditTime: 2024-10-08 20:30:05
  */
 import defaultSettings from '@/settings.ts'
 
-const title = defaultSettings.title || '全景漫游'
+const title = defaultSettings.title || '图形可视化编辑器'
 
 /**
  * @param {String} pageTitle
